7ad77e1ed027c79dc9ca5fc30e123d0add101c5d
John Sloan <jsloanphoto@gmail.com>
2023-04-06 16:21:25 -0400

    first exercise close to finsih, have to go to work

--- 1/courseinformation/src/App.js
+++ 1/courseinformation/src/App.js
@@ -1,11 +1,14 @@
 const App = () => {
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const sections =[
+    {part1 : 'Fundamentals of React', exercises1 : 10},
+    {part2 : 'Using props to pass data', exercises2 : 7},
+    {part3 : 'State of a component', exercises3 : 14}
+  ]
+
+  const math = (10 + 7 + 14)
 
   const Header = (props) => {
     console.log(props)
@@ -15,11 +18,21 @@ const App = () => {
       </div>
     )
   }
-  const Content = () => {
-  
-  }
-  const Total = () => {
-    
+    const Content = (props) => {
+      console.log(props)
+      return(
+        <div>
+        <p>{sections[0].part1} {sections[0].exercises1}</p>
+        <p>{sections[1].part2} {sections[1].exercises2}</p>
+        <p>{sections[2].part3} {sections[2].exercises3}</p>
+        </div>
+      )
+    }
+  const Total = (props) => {
+    console.log(props)
+    return(
+      <p>{math}</p>
+    )
   }
   return (
     <div>
