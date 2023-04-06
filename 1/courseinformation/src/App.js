fb3ad21fd879328406e9c96dd16692133ce768db
John Sloan <jsloanphoto@gmail.com>
2023-04-06 15:35:29 -0400

    working through first exercise

---
+++ 1/courseinformation/src/App.js
@@ -0,0 +1,33 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    console.log(props)
+    return(
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+  const Content = () => {
+  
+  }
+  const Total = () => {
+    
+  }
+  return (
+    <div>
+      <Header course={course}/>
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
