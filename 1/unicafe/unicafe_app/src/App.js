eb7c8dbff2ebab1329825aac5f20a9d4520e246e
John Sloan <jsloanphoto@gmail.com>
2023-04-13 13:41:47 -0400

    started on exercise 1.6 setup to get layout of app, have to go to rockhouse soon so taking a break

--- 1/unicafe/unicafe_app/src/App.js
+++ 1/unicafe/unicafe_app/src/App.js
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 
-const App = () => {
+const App = (props) => {
+  console.log(props)
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Header = () =>{
+    return(
+      <div>
+        <h1>Give Feedback</h1>
+      </div>
+    )
+
+  }
+  const Button = (props) =>{
+    console.log(props)
+    return(
+      <div>
+        <button>button</button>
+      </div>
+    )
+  }
+
   return (
     <div>
-      code here
+      <Header />
+      <Button />
+      <Button />
+      <Button />
     </div>
   )
 }
