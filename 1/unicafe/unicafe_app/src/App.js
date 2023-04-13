8c830787ceff536998be1a8a4c62d6305220b609
John Sloan <jsloanphoto@gmail.com>
2023-04-13 16:42:10 -0400

    work after run and before going to Rockhouse : work on exercise 1.6 : got buttons layed out, need further workings on how they parse information

--- 1/unicafe/unicafe_app/src/App.js
+++ 1/unicafe/unicafe_app/src/App.js
@@ -7,10 +7,23 @@ const App = (props) => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setToGoodValue = (newGoodValue) => () => {
+    console.log('value now', newGoodValue)
+    setGood(newGoodValue)
+  }
+  const setToNeutralValue = (newNeutralValue) => () => {
+    console.log('value now', newNeutralValue)
+    setNeutral(newNeutralValue)
+  }
+  const setToBadValue = (newBadValue) => () => {
+    console.log('value now', newBadValue)
+    setBad(newBadValue)
+  }
+
   const Header = () =>{
     return(
       <div>
-        <h1>Give Feedback</h1>
+        <h1>Please Give Feedback</h1>
       </div>
     )
 
@@ -19,7 +32,9 @@ const App = (props) => {
     console.log(props)
     return(
       <div>
-        <button>button</button>
+        <button>
+          {props.text}
+        </button>
       </div>
     )
   }
@@ -27,9 +42,9 @@ const App = (props) => {
   return (
     <div>
       <Header />
-      <Button />
-      <Button />
-      <Button />
+      <Button onClick={setToGoodValue(good + 1)} text='good'/>
+      <Button onClick={setToNeutralValue(neutral + 1)} text='neutral'/>
+      <Button onClick={setToBadValue(bad + 1)} text='bad'/>
     </div>
   )
 }
