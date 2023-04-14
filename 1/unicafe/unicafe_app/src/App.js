1bd689cd9822cd3df55e2ca72bb671841e4533f0
John Sloan <jsloanphoto@gmail.com>
2023-04-14 00:20:09 -0400

    working template for the end of exercise 1.6 : unicafe step1

--- 1/unicafe/unicafe_app/src/App.js
+++ 1/unicafe/unicafe_app/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+// const Display = props => <div>{props.value}</div>
+
 const Header = (props) => {
   return(
     <div>
@@ -12,9 +14,6 @@ const Footer = () => {
   return(
     <div>
       <h2>Statistics</h2>
-      <p>good</p>
-      <p>neutral</p>
-      <p>bad</p>
     </div>
   )
 }
@@ -30,7 +29,6 @@ const Button = (props) =>{
 
 const App = () => {
 
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -70,13 +68,18 @@ const App = () => {
 
     setTotal(updatedBad + neutral + good)
   }
+
   return (
     <div>
+      {/* <Display value={total}/> */}
       <Header text='Please Give Feedback'/>
       <Button handleClick={handleGoodClick} text='good'/>
       <Button handleClick={handleNeutralClick} text='neutral'/>
       <Button handleClick={handleBadClick} text='bad'/>
       <Footer />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
       <p>total {total}</p>
     </div>
   )
