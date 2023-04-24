52ded0cb5b34d3f7d3ef2427d0cf710543bbc934
John Sloan <jsloanphoto@gmail.com>
2023-04-24 17:12:56 -0400

    work on 2b up to Filtering Displayed Elements : found in 2.a.examples_work

--- 2/a/examples_work/src/App.js
+++ 2/a/examples_work/src/App.js
@@ -1,24 +1,43 @@
+import { useState} from 'react'
 import Note from "./components/Notes"
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note..'
+  )
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content : newNote,
+      important : Math.random() < 0.5,
+      id : notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
-      <ul>
-{/* following way not recommended: */}
-      {/* <ul>
-        {notes.map((note, i) =>
-        <li key={i}>
-          {note.content}
-        </li>)}
-      </ul> */}
-{/* recommended: */}
       <ul>
         {notes.map(note =>
         <Note key={note.id} note={note} />
         )}
       </ul>
-      </ul>
+      <form onSubmit={addNote}>
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
