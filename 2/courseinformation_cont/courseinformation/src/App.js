0838c90924ca0b79985b5fe40539eb9f178e64a6
John Sloan <jsloanphoto@gmail.com>
2023-04-24 14:33:55 -0400

    set up components

--- 2/courseinformation_cont/courseinformation/src/App.js
+++ 2/courseinformation_cont/courseinformation/src/App.js
@@ -1,7 +1,9 @@
+import { Course } from "./components/Course"
+
 const App = () => {
   const course = {
-    id: 1,
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
@@ -17,57 +19,15 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name : 'Redux',
+        exercises : 11,
+        id: 4
       }
     ]
   }
 
   return <Course course={course} />
-}
-
-const Course = (props) => {
-  console.log('COURSE props =', props);
-  const {course} = props
-  return(
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-const Header = (props) => {
-  const {name} = props
-  return(
-    <h1>{name}</h1>
-  )
-}
-const Content = (props) => {
-  console.log('CONTENT props =', props);
-  const {parts} = props
-  return(
-    <div>
-      {parts.map(part =>(
-        <Part key={part.id} name={part.name} exercises={part.exercises}/>
-      ))}
-    </div>
-  )
-}
-const Part = (props) => {
-  console.log('PART props =', props);
-  const {name, exercises} = props
-  return (
-    <p>{name} : {exercises}</p>
-  )
-}
-const Total = (props) => {
-  console.log('TOTAL props =', props)
-  const {parts} = props
-  const totalExercises = parts.reduce((total,part) => total + part.exercises, 0)
-
-  return(
-    <div>
-      <p>Total Exercises : {totalExercises}</p>
-    </div>
-  )
 }
 export default App
---
+++ 2/courseinformation_cont/courseinformation/src/components/Course.js
@@ -0,0 +1,50 @@
+const Course = (props) => {
+    console.log('COURSE props =', props);
+    const {course} = props
+    return(
+      <div>
+        <Header name={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>
+      </div>
+    )
+  }
+  const Header = (props) => {
+    const {name} = props
+    return(
+      <div>
+      <h1>{name}</h1>
+      </div>
+    )
+  }
+  const Content = (props) => {
+    console.log('CONTENT props =', props);
+    const {parts} = props
+    return(
+      <div>
+        {parts.map(part =>(
+          <Part key={part.id} name={part.name} exercises={part.exercises}/>
+        ))}
+      </div>
+    )
+  }
+  const Part = (props) => {
+    console.log('PART props =', props);
+    const {name, exercises} = props
+    return (
+      <p>{name} : {exercises}</p>
+    )
+  }
+  const Total = (props) => {
+    console.log('TOTAL props =', props)
+    const {parts} = props
+    const totalExercises = parts.reduce((total,part) => total + part.exercises, 0)
+  
+    return(
+      <div>
+        <p>Total Exercises : {totalExercises}</p>
+      </div>
+    )
+  }
+
+  export {Course};
